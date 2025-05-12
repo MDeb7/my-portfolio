f98ed128ed4f01a9e46f1968577ac704c4780a98
Debjyoti <majumderdebjyoti8@example.com>
2025-05-12 14:14:53 +0530

    a new project added

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+import styles from "./Hero.module.css";
+import { getImageUrl } from "../../utils";
+
+export const Hero = () => {
+  return (
+    <section className={styles.container}>
+      <div className={styles.content}>
+        <h1 className={styles.title}>Hi, I'm Debjyoti</h1>
+        <p className={styles.description}>
+          I'm a novice full-stack developer using React and
+          NodeJS. Interested in Machine Learning as well. Looking for opportunity in the corporate world in these field!
+        </p>
+        <a href="mailto:majumderdebjyoti8@gmail.com" className={styles.contactBtn}>
+          Contact Me
+        </a>
+      </div>
+      <img
+        src={getImageUrl("hero/heroImage.png")}
+        alt="Hero image of me"
+        className={styles.heroImg}
+      />
+      <div className={styles.topBlur} />
+      <div className={styles.bottomBlur} />
+    </section>
+  );
+};
